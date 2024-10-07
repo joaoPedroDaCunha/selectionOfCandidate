8fd854e9794e13548cd75358e2c4b6490bd92fc9
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-07 12:57:59 -0300

    print

--- src/Entity/Candidete.java
+++ src/Entity/Candidete.java
@@ -6,10 +6,13 @@ public class Candidete {
 
     protected static Double salaryBase = 2000.0;
 
+    static String selectedCandidates [];
+
     public static void selectionOfCandidates(){
         String [] candidates = {"marcio","jose","marcos","felipe","monica","cebolina","cascão","magali","augusto","paulo"};
         int selectioncandidates = 0;
         int currentCandidate = 0;
+        int count = 0;
         while(selectioncandidates < 5 && currentCandidate < candidates.length){
             String candidate = candidates [ currentCandidate ];
             double salaryWanted = valueWanted();
@@ -17,8 +20,17 @@ public class Candidete {
             if(salaryBase >= salaryWanted  ){
                 System.out.println("O candidato :"+candidates+"foi selecionando para vaga");
                 selectioncandidates ++;
+                selectedCandidates [count] = candidate;
+                count ++;
             }
             currentCandidate ++;
+        }
+    }
+
+    public static void print(){
+        System.out.println("Imprimindo a lista de candidatos selecionandos :");
+        for(int i=0;i<=selectedCandidates.length;i++){
+            System.out.println(selectedCandidates[i]);
         }
     }
 
