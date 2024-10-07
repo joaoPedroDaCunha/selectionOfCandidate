60578445bf500a6b99e607424f209403a5a5df2b
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-07 13:34:00 -0300

    getInTouch

--- src/Entity/Candidete.java
+++ src/Entity/Candidete.java
@@ -1,5 +1,6 @@
 package Entity;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Candidete {
@@ -7,6 +8,7 @@ public class Candidete {
     protected static Double salaryBase = 2000.0;
 
     static String selectedCandidates [];
+    static double selectedSalary [];
 
     public static void selectionOfCandidates(){
         String [] candidates = {"marcio","jose","marcos","felipe","monica","cebolina","cascão","magali","augusto","paulo"};
@@ -21,6 +23,7 @@ public class Candidete {
                 System.out.println("O candidato :"+candidates+"foi selecionando para vaga");
                 selectioncandidates ++;
                 selectedCandidates [count] = candidate;
+                selectedSalary [count] = salaryWanted;
                 count ++;
             }
             currentCandidate ++;
@@ -34,18 +37,47 @@ public class Candidete {
         }
     }
 
-    public static void analyzeCandidate(Double salaryWanted){
-        if(salaryBase < salaryBase ){
-            System.out.println("Ligar para o candidato");
-        }else if(salaryBase == salaryWanted){
-            System.out.println("Ligar Para o candidato com contra proposta");
-        }else{
-            System.out.println("Aguardando o resultado dos demais candidatos");
+    public static void analyzeCandidate(){
+        
+        for(int i=0;i <= selectedCandidates.length;i++){
+            if(salaryBase < selectedSalary[i] ){
+                System.out.println("Ligar para o candidato :"+selectedCandidates[i]);
+            }else if(salaryBase == selectedSalary[i]){
+                System.out.println("Ligar Para o candidato :"+selectedCandidates[i]+" com contra proposta");
+            }else{
+                System.out.println("Aguardando o resultado dos demais candidatos");
+            }
         }
 
     }
+    static public void getInTouch(String candidate){
+        int attemptsMade = 1;
+        boolean keepTrying = true;
+        boolean answered = false;
+        do {
+            answered = answer();
+            keepTrying = !answered;
+            if (keepTrying) {
+                attemptsMade ++;
+            }else{
+                System.out.println("Contato realizado com sucesso");
+            }
+        }while(answered == false && attemptsMade <= 3);
+
+        if (answered) {
+            System.out.println("Conseguimos contato com "+candidate+" Na "+attemptsMade+" tentativa realizada");
+        }else{
+            System.out.println("Não Conseguimos contato com "+candidate+", número maximo de tentativas realizadas");
+        }
+    }
+
+   
 
     static double valueWanted(){
         return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
+    static boolean answer(){
+        return new Random().nextInt(3)==1;
+    }
+    
 }
