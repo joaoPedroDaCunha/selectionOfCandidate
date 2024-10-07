5d7528728b5cd93bdda2558224861972c464360c
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-07 12:09:50 -0300

    Candidete

---
+++ src/Entity/Candidete.java
@@ -0,0 +1,16 @@
+package Entity;
+
+public class Candidete {
+
+    public static void analyzeCandidate(Double salaryWanted){
+        Double salaryBase = 2000.0;
+        if(salaryBase < salaryBase ){
+            System.out.println("Ligar para o candidato");
+        }else if(salaryBase == salaryWanted){
+            System.out.println("Ligar Para o candidato com contra proposta");
+        }else{
+            System.out.println("Aguardando o resultado dos demais candidatos");
+        }
+
+    }
+}
---
+++ src/Program/Program.java
@@ -0,0 +1,12 @@
+package Program;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Processo seletivo");
+        sc.close();
+    }
+    
+}
